e15ff6c76df8f4227af3659262a2b51bdee4b085
antodima <antonio.di.mauro.23@gmail.com>
2021-10-27 15:54:06 +0200

    updated gmres

--- gmres.m
+++ gmres.m
@@ -1,22 +1,41 @@
 % GMRES for large and sparse linear system
 
 m = 1000;
+n = 50;
 A = 10*speye(m) + sprandn(m, m, 0.01);
 b = randn(size(A,1), 1);
-[Q, H] = arnoldi(A, b, 50);
-r = nan(50,1);
+[Q, H] = arnoldi(A, b, n);
+r = nan(n,1);
 % tests several ’slices’ of Q,H
 % this simulates Arnoldi with various values of n.
-for n = 1:50
-    y = H(1:n+1, 1:n) \ eye(n+1, 1) * norm(b);
-    x = Q(:, 1:n) * y;
-    r(n) = norm(A*x - b);
+for i = 1:n
+    y = H(1:i+1, 1:i) \ eye(i+1, 1) * norm(b);
+    x = Q(:, 1:i) * y;
+    r(i) = norm(A*x - b);
 end
 semilogy(r)
 
 
 
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [Q, H] = arnoldi(A, b, n)
+    m = length(A); % rows of A
+    Q = zeros(length(b), n);
+    Q(:,1) = b / norm(b);
+    H = zeros(min(n+1,n), m);
+
+    for k=1:n
+        w = A * Q(:,k);
+        for i=1:k
+            H(i,k) = Q(:,i)' * w;
+            w = w - H(i,k) * Q(:,i);
+        end
+       H(k+1,k) = norm(w);
+       Q(:,k+1) = w / H(k+1,k);
+    end
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [v, s] = householder_vector(x)
     s = -sgn(x(1)) * norm(x);
@@ -33,25 +52,5 @@ function [Q, A] = myqr(A)
         A(j,j) = s; A(j+1:end,j) = 0;
         A(j:end,j+1:end) = A(j:end,j+1:end) - 2*v*(v'*A(j:end,j+1:end));
         Q(:, j:end) = Q(:, j:end) - Q(:,j:end)*v*2*v';
-    end
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [Q, H] = arnoldi(A, b, n)
-    m = length(A);
-    Q = zeros(length(b), n);
-    Q(:,1) = b / norm(b);
-    H = zeros(min(n+1,n),m);
-
-    for k=1:n
-        z = A * Q(:,k);
-        for i=1:k
-            H(i,k) = Q(:,i)' * z;
-            z = z - H(i,k) * Q(:,i);
-        end
-        if k < m
-           H(k+1,k) = norm(z);
-           if H(k+1,k) == 0, return, end
-           Q(:,k+1) = z / H(k+1,k);
-        end
     end
 end
